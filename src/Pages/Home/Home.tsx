d50f857e0bae088567386f3840ef2ef22dac20b8
Zephinax <Zephinax@gmail.com>
2024-05-27 23:21:42 +0330

    feat: update home and use car wrapper

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,39 +2,25 @@ import './Home.css';
 import './Animations.css';
 import Ads from '../../Components/Ads/Ads';
 import Filter from '../../Components/Filter/Filter';
-import Popular from '../../Components/Popular/Popular';
-import Recommendation from '../../Components/Recommendation/Recommendation';
-import { useEffect, useState } from 'react';
-import LoadingSpiner from '../../Components/LoadingSpiner/LoadingSpiner';
+import CarWrapper from '../../Components/CarWrapper/CarWrapper';
 
 export default function Home() {
-  const [adsLoad, setAdsLoad] = useState(false);
-  const [popLoad, setPopLoad] = useState(false);
-  const [recLoad, setRecLoad] = useState(false);
-  const [allLoad, setAllLoad] = useState(false);
-
-  useEffect(() => {
-    const arr = [adsLoad, popLoad, recLoad];
-    arr.every(Boolean) && setAllLoad(true);
-  }, [adsLoad, popLoad, recLoad]);
-
-  const statusToggle = (comp: string) => {
-    if (comp === 'Ads') {
-      setAdsLoad(true);
-    } else if (comp === 'Popular') {
-      setPopLoad(true);
-    } else if (comp === 'Recommendation') {
-      setRecLoad(true);
-    }
-  };
-
   return (
     <div className='app-container'>
-      <Ads setLoad={statusToggle} />
+      <Ads />
       <Filter />
-      <Popular setLoad={statusToggle} />
-      <Recommendation setLoad={statusToggle} />
-      {!allLoad && <LoadingSpiner />}
+      <CarWrapper
+        title='Sport Cars'
+        linkTitle='View All'
+        link='/Category'
+        url='http://localhost:8000/Cars?category=Sport'
+      />
+      <CarWrapper
+        title='All Cars'
+        linkTitle='View All'
+        link='/Category'
+        url='http://localhost:8000/Cars'
+      />
     </div>
   );
 }
