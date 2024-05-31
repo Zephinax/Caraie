352e700d3b34cf74260fda800e08df760952eb1a
Zephinax <Zephinax@gmail.com>
2024-05-31 19:34:13 +0330

    feat: add detail routing and fix category route

--- src/Pages/Routes.tsx
+++ src/Pages/Routes.tsx
@@ -1,10 +1,12 @@
 import Home from './Home/Home';
 import Category from './Category/Category';
+import CarDetail from './CarDetail/CarDetail';
 import { RouteObject, Navigate } from 'react-router-dom';
 const Routes: RouteObject[] = [
   { path: '/', element: <Home /> },
   { path: '*', element: <Navigate to='/' /> },
-  { path: '/Category', element: <Category /> },
+  { path: '/detail/:CarId', element: <CarDetail /> },
+  { path: '/category', element: <Category /> },
 ];
 
 export default Routes;
