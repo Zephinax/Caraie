86efe26a61f282232bc9d74b4145d28e9d36d583
Zephinax <Zephinax@gmail.com>
2024-06-04 22:18:59 +0330

    fix: some tweaks in loader

--- src/Components/LoadingSpiner/LoadingSpiner.tsx
+++ src/Components/LoadingSpiner/LoadingSpiner.tsx
@@ -3,17 +3,6 @@ import './LoadingSpiner.css';
 export default function LoadingSpiner() {
   return (
     <div className='loader-container'>
-      {/* <div className='loader'>
-        <div className='square' id='sq1'></div>
-        <div className='square' id='sq2'></div>
-        <div className='square' id='sq3'></div>
-        <div className='square' id='sq4'></div>
-        <div className='square' id='sq5'></div>
-        <div className='square' id='sq6'></div>
-        <div className='square' id='sq7'></div>
-        <div className='square' id='sq8'></div>
-        <div className='square' id='sq9'></div>
-      </div> */}
       <div className='loader'>
         Loading
         <span></span>
