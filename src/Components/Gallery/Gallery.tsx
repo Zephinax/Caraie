cbad956b0790a0212376406b5060487c0f76deb1
Zephinax <Zephinax@gmail.com>
2024-09-28 19:24:47 +0330

    feat: add image slider to car detail

--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import insidePic11 from './../../../public/Images/carsInside/Carinside1.jpg';
+import insidePic12 from './../../../public/Images/carsInside/Carinside2.jpg';
+import insidePic13 from './../../../public/Images/carsInside/Carinside3.jpg';
+import insidePic14 from './../../../public/Images/carsInside/Carinside4.jpg';
 import './Gallery.css';
 
 interface Car {
@@ -7,38 +13,60 @@ interface Car {
 interface GalleryProps {
   data: Car;
 }
+
+const images = [
+  {
+    original: insidePic11,
+    thumbnail: insidePic11,
+  },
+  {
+    original: insidePic12,
+    thumbnail: insidePic12,
+  },
+  {
+    original: insidePic13,
+    thumbnail: insidePic13,
+  },
+  {
+    original: insidePic14,
+    thumbnail: insidePic14,
+  },
+];
 export default function Gallery(props: GalleryProps) {
-  const [mainImage] = useState('default');
-  const carAssets = [1, 2, 3];
+  // const [mainImage] = useState('default');۱
+  // const carAssets = [1, 2, 3];
+
+  const [galleryImages, setGalleryImages] = useState(images);
   return (
-    <div className='gallery-wrapper user-select-none'>
-      <div className='main-img'>
-        <img
-          style={{ borderRadius: '10px' }}
-          src={
-            mainImage === 'default'
-              ? new URL(
-                  `/public/images/cars/${props.data.img}`,
-                  import.meta.url
-                ).href
-              : new URL(`/public/images/noImg.webp`, import.meta.url).href
-          }
-          alt=''
-        />
-      </div>
-      <div className='car-assets'>
-        {carAssets.map((image) => {
-          const src = new URL(
-            `/public/images/carsInside/Carinside${image}.jpg`,
-            import.meta.url
-          ).href;
-          return (
-            <div className='image-container' key={image}>
-              <img src={src} alt='' />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    // <div className='gallery-wrapper user-select-none'>
+    //   <div className='main-img'>
+    //     <img
+    //       style={{ borderRadius: '10px' }}
+    //       src={
+    //         mainImage === 'default'
+    //           ? new URL(
+    //               `/public/images/cars/${props.data.img}`,
+    //               import.meta.url
+    //             ).href
+    //           : new URL(`/public/images/noImg.webp`, import.meta.url).href
+    //       }
+    //       alt=''
+    //     />
+    //   </div>
+    //   <div className='car-assets'>
+    //     {carAssets.map((image) => {
+    //       const src = new URL(
+    //         `/public/images/carsInside/Carinside${image}.jpg`,
+    //         import.meta.url
+    //       ).href;
+    //       return (
+    //         <div className='image-container' key={image}>
+    //           <img src={src} alt='' />
+    //         </div>
+    //       );
+    //     })}
+    //   </div>
+    // </div>
+    <ImageGallery items={galleryImages} />
   );
 }
